f9cf7c492295d14757bd4e9d2ff926509cf79d5c
Aleksander Dudek <dudek.olek4@gmail.com>
2017-04-18 02:25:07 +0200

    fisr push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule, JsonpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {WeatherComponent} from './weather-widget/component/weather.component';
+
+import {SpeedUnitPipe} from './weather-widget/pipe/speed-unit.pipe';
+import {TempUnitPipe} from './weather-widget/pipe/temp-unit.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    SpeedUnitPipe,
+    TempUnitPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
